65e523cad43662648c9b59a6d1e000cc0ae1d32a
Alberto <albertus82@users.noreply.github.com>
2021-09-27 22:14:33 +0200

    Rewritten.

--- src/main/java/com/github/albertus82/extfix/ExtFix.java
+++ src/main/java/com/github/albertus82/extfix/ExtFix.java
@@ -1,6 +1,8 @@
 package com.github.albertus82.extfix;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,9 +13,13 @@ import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
+import java.util.stream.Stream;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOCase;
+import org.apache.commons.io.file.PathUtils;
+import org.apache.commons.io.filefilter.CanReadFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.tika.Tika;
 import org.apache.tika.config.TikaConfig;
 import org.apache.tika.mime.MimeTypeException;
@@ -29,7 +35,7 @@ import picocli.CommandLine.Parameters;
 @Command
 public class ExtFix implements Callable<Integer> {
 
-	private static final String[] EXTENSIONS = { "png", "PNG", "jpg", "JPG", "jpeg", "JPEG" };
+	private static final String[] SUFFIXES = { ".png", ".jpg", ".jpeg" };
 
 	final TikaConfig tikaConfig = TikaConfig.getDefaultConfig();
 	final Tika tika = new Tika(tikaConfig);
@@ -44,31 +50,39 @@ public class ExtFix implements Callable<Integer> {
 		System.exit(new CommandLine(new ExtFix()).setCommandName(ExtFix.class.getSimpleName().toLowerCase(Locale.ROOT)).setOptionsCaseInsensitive(true).execute(args));
 	}
 
+	private int count = 0;
+
 	@Override
 	public Integer call() throws IOException {
+		basePath = basePath.toFile().getCanonicalFile().toPath();
 		final Map<String, String> renames = new TreeMap<>();
-		FileUtils.streamFiles(basePath.toFile(), true, EXTENSIONS).forEach(f -> {
+		final Stream<Path> stream = PathUtils.walk(basePath, CanReadFileFilter.CAN_READ.and(new SuffixFileFilter(SUFFIXES, IOCase.INSENSITIVE)), Short.MAX_VALUE, false, FileVisitOption.FOLLOW_LINKS);
+		stream.filter(path -> path.getFileName() != null).forEach(path -> {
+			count++;
+			final File file = path.toFile();
 			try {
-				final String extension = FilenameUtils.getExtension(f.getName());
-				final String mediaType = tika.detect(f);
+				final String extension = FilenameUtils.getExtension(file.getName());
+				final String mediaType = tika.detect(path);
 				if (mediaType == null) {
-					log.log(Level.WARNING, "Cannot determine type of {0}.", f);
+					log.log(Level.WARNING, "Cannot determine type of ''{0}''.", path);
 				}
 				else {
 					final List<String> extensions = tikaConfig.getMimeRepository().forName(mediaType).getExtensions();
-					log.log(Level.FINE, "{0} - {1}", new Object[] { extensions, f });
+					log.log(Level.FINE, "{0} - {1}", new Object[] { extensions, path });
 					if (!extensions.isEmpty() && extensions.stream().noneMatch(e -> e.equalsIgnoreCase('.' + extension))) {
-						final String oldName = f.getCanonicalPath();
-						final String newName = f.getCanonicalPath() + extensions.get(0);
+						final String oldName = file.getCanonicalPath();
+						final String newName = file.getCanonicalPath() + extensions.get(0);
 						renames.put(oldName, newName);
 						log.log(Level.FINE, "{0} -> {1}", new String[] { oldName, newName });
 					}
 				}
 			}
 			catch (final MimeTypeException | RuntimeException | IOException e) {
-				log.log(Level.WARNING, e, () -> "Skipped " + f + ':');
+				log.log(Level.WARNING, e, () -> "Skipped '" + path + "':");
 			}
 		});
+
+		log.log(Level.INFO, "{0} files analyzed.", count);
 
 		for (final Entry<String, String> e : renames.entrySet()) {
 			log.log(Level.INFO, "{0} -> {1}", new String[] { e.getKey(), e.getValue() });
