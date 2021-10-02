0925374a38ff2a78582aec7df916fccd46202d41
Alberto <albertus82@users.noreply.github.com>
2021-10-02 21:03:10 +0200

    walkFileTree

--- src/main/java/com/github/albertus82/extfix/ExtFix.java
+++ src/main/java/com/github/albertus82/extfix/ExtFix.java
@@ -2,21 +2,23 @@ package com.github.albertus82.extfix;
 
 import java.io.IOException;
 import java.nio.file.FileVisitOption;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
-import java.util.stream.Stream;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOCase;
-import org.apache.commons.io.file.PathUtils;
 import org.apache.commons.io.filefilter.CanReadFileFilter;
 import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.tika.Tika;
@@ -81,58 +83,86 @@ public class ExtFix implements Callable<Integer> {
 		final List<String> suffixes = extensions.get();
 		System.out.println("Extensions: " + suffixes + '.');
 
-		final List<FileVisitOption> option = new ArrayList<>();
-		if (links) {
-			option.add(FileVisitOption.FOLLOW_LINKS);
-		}
-
-		final Stream<Path> stream = PathUtils.walk(basePath, CanReadFileFilter.CAN_READ.and(new SuffixFileFilter(suffixes, IOCase.INSENSITIVE)), Short.MAX_VALUE, false, option.toArray(new FileVisitOption[option.size()]));
-
 		System.out.print("Analyzing... ");
 		System.out.print(getWaitChar());
 
 		final Map<Path, Path> renames = new TreeMap<>();
-		stream.filter(path -> path.getFileName() != null).forEach(p -> {
-			try {
-				final Path path = p.toFile().getCanonicalFile().toPath();
-				final String mediaType = tika.detect(path);
-				if (mediaType == null) {
-					System.out.println();
-					System.out.println("Cannot determine type of '" + path + "'.");
-					System.out.print("Analyzing... ");
-				}
-				else {
-					final List<String> exts = tikaConfig.getMimeRepository().forName(mediaType).getExtensions();
-					log.debug("{} <- {}", exts, path);
-					if (exts.isEmpty()) {
-						System.out.println();
-						System.out.println("Cannot determine extension for '" + path + "'.");
-						System.out.print("Analyzing... ");
-					}
-					else {
-						final Optional<Path> fixed = fixFileName(path, exts);
-						if (fixed.isPresent()) {
-							renames.put(path, fixed.get());
+		Files.walkFileTree(basePath, links ? EnumSet.of(FileVisitOption.FOLLOW_LINKS) : Collections.emptySet(), Short.MAX_VALUE, new FileVisitor<Path>() {
+			@Override
+			public FileVisitResult preVisitDirectory(@NonNull final Path dir, final BasicFileAttributes attrs) {
+				return FileVisitResult.CONTINUE;
+			}
+
+			@Override
+			public FileVisitResult visitFile(@NonNull final Path p, final BasicFileAttributes attrs) {
+				if (FileVisitResult.CONTINUE.equals((new SuffixFileFilter(suffixes, IOCase.INSENSITIVE)).accept(p, attrs))) {
+					if (FileVisitResult.CONTINUE.equals(CanReadFileFilter.CAN_READ.accept(p, attrs))) {
+						try {
+							final Path path = p.toFile().getCanonicalFile().toPath();
+							final String mediaType = tika.detect(path);
+							if (mediaType == null) {
+								System.out.println();
+								System.out.println("Cannot determine type of '" + path + "'.");
+								System.out.print("Analyzing... ");
+							}
+							else {
+								final List<String> exts = tikaConfig.getMimeRepository().forName(mediaType).getExtensions();
+								log.debug("{} <- {}", exts, path);
+								if (exts.isEmpty()) {
+									System.out.println();
+									System.out.println("Cannot determine extension for '" + path + "'.");
+									System.out.print("Analyzing... ");
+								}
+								else {
+									final Optional<Path> fixed = fixFileName(path, exts);
+									if (fixed.isPresent()) {
+										renames.put(path, fixed.get());
+										System.out.println();
+										System.out.println("Found " + FilenameUtils.getExtension(fixed.get().toString()).toUpperCase() + " file with wrong extension: '" + path + "'.");
+										System.out.print("Analyzing... ");
+									}
+									else {
+										System.out.print('\b');
+									}
+								}
+							}
+							count++;
+							System.out.print(getWaitChar());
+						}
+						catch (final MimeTypeException | IOException | RuntimeException e) {
 							System.out.println();
-							System.out.println("Found " + FilenameUtils.getExtension(fixed.get().toString()).toUpperCase() + " file with wrong extension: '" + path + "'.");
+							if (errors) {
+								e.printStackTrace();
+							}
+							System.out.println("Skipping '" + p + "' due to an exception: " + e);
 							System.out.print("Analyzing... ");
 						}
-						else {
-							System.out.print('\b');
-						}
+					}
+					else {
+						System.out.println();
+						System.out.println("Skipping not readable file '" + p + "'.");
+						System.out.print("Analyzing... ");
 					}
 				}
-				count++;
+				return FileVisitResult.CONTINUE;
 			}
-			catch (final MimeTypeException | IOException | RuntimeException e) {
+
+			@Override
+			public FileVisitResult visitFileFailed(@NonNull final Path file, final IOException e) {
 				System.out.println();
 				if (errors) {
 					e.printStackTrace();
 				}
-				System.out.println("Skipped '" + p + "': " + e);
+				System.out.println("Skipping '" + file + "' due to an exception: " + e);
 				System.out.print("Analyzing... ");
+				System.out.print(getWaitChar());
+				return FileVisitResult.CONTINUE;
 			}
-			System.out.print(getWaitChar());
+
+			@Override
+			public FileVisitResult postVisitDirectory(@NonNull final Path dir, final IOException e) {
+				return FileVisitResult.CONTINUE;
+			}
 		});
 
 		System.out.println();
