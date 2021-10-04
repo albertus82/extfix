7314dac1f80977689c0ed86c5dadaef44338a310
Alberto <albertus82@users.noreply.github.com>
2021-10-04 20:48:04 +0200

    Test.

--- src/test/java/com/github/albertus82/extfix/engine/RenamerTest.java
+++ src/test/java/com/github/albertus82/extfix/engine/RenamerTest.java
@@ -1,36 +1,38 @@
-//package com.github.albertus82.extfix.engine;
-//
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.Collections;
-//import java.util.Map;
-//import java.util.UUID;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import com.github.albertus82.extfix.Console;
-//
-//class RenamerTest {
-//
-//	private static final String RANDOM = UUID.randomUUID().toString().replace("-", "");
-//
-//	@Test
-//	void testConstructor() {
-//		Assertions.assertThrows(NullPointerException.class, () -> new Renamer(null, true));
-//		Assertions.assertDoesNotThrow(() -> new Renamer(new Console(), true));
-//		Assertions.assertDoesNotThrow(() -> new Renamer(new Console(), false));
-//	}
-//
-//	@Test
-//	void testRename() {
-//		final Renamer renamer = new Renamer(new Console(), false);
-//		final Map<Path, String> m1 = Collections.singletonMap(null, null);
-//		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m1));
-//		final Map<Path, String> m2 = Collections.singletonMap(null, "ext");
-//		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m2));
-//		final Map<Path, String> m3 = Collections.singletonMap(Paths.get("/tmp/" + RANDOM + ".bar"), null);
-//		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m3));
-//	}
-//
-//}
+package com.github.albertus82.extfix.engine;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.github.albertus82.extfix.Console;
+
+class RenamerTest {
+
+	private static final String RANDOM = UUID.randomUUID().toString().replace("-", "");
+
+	@Test
+	void testConstructor() {
+		Assertions.assertThrows(NullPointerException.class, () -> new Renamer(null));
+		Assertions.assertDoesNotThrow(() -> new Renamer(new Console()));
+	}
+
+	@Test
+	void testRename() {
+		final Renamer renamer = new Renamer(new Console());
+		final Map<Path, String> m1 = Collections.singletonMap(null, null);
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m1, false));
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m1, true));
+		final Map<Path, String> m2 = Collections.singletonMap(null, "ext");
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m2, false));
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m2, true));
+		final Map<Path, String> m3 = Collections.singletonMap(Paths.get("/tmp/" + RANDOM + ".bar"), null);
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m3, false));
+		Assertions.assertThrows(NullPointerException.class, () -> renamer.rename(m3, true));
+	}
+
+}
